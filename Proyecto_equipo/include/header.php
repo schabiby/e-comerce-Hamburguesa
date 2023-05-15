cda7da7b785f8373ea9f2bdd5b160c3bca0282b4
schab <schabibyd@gmail.com>
2023-05-15 14:00:29 -0600

    biblioteca

--- Proyecto_equipo/include/header.php
+++ Proyecto_equipo/include/header.php
@@ -7,7 +7,7 @@
 
     <!-- font awesome cdn link  -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
-
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.0.0/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.css">
 
     <!-- custom css file link  -->
