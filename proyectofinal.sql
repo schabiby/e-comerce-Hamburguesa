62b35a8265f47b09f04989a6f122505c96e76eb9
AmFlowersUwU <85804801+AmFlowersUwU@users.noreply.github.com>
2023-05-22 07:49:45 -0600

    Versión usada de la BD

--- proyectofinal.sql
+++ proyectofinal.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 14-05-2023 a las 20:39:57
+-- Tiempo de generación: 22-05-2023 a las 15:46:19
 -- Versión del servidor: 10.4.27-MariaDB
 -- Versión de PHP: 8.2.0
 
@@ -31,8 +31,21 @@ CREATE TABLE `catalogo` (
   `ID_Producto` int(11) NOT NULL,
   `Costo` float NOT NULL,
   `Descripción` varchar(120) NOT NULL,
-  `RutaImagen` varchar(120) NOT NULL
+  `RutaImagen` varchar(120) NOT NULL,
+  `hasBeenSold` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Volcado de datos para la tabla `catalogo`
+--
+
+INSERT INTO `catalogo` (`ID_Producto`, `Costo`, `Descripción`, `RutaImagen`, `hasBeenSold`) VALUES
+(1, 1.99, 'Hamburguesa con queso', 'images/product-6.png', 150),
+(2, 3.5, 'Hamburguesa doble con queso', 'images/product-4.png', 280),
+(3, 1.99, 'Hamburguesa de pollo', 'images/product-3.png', 100),
+(4, 5.99, 'Cuarto de libra doble con queso', 'images/product-1.png', 140),
+(5, 1.5, 'Desayuno huevo con tocino', 'images/product-2.png', 80),
+(6, 2.15, 'Hamburguesa de pollo con queso', 'images/product-5.png', 115);
 
 -- --------------------------------------------------------
 
@@ -45,9 +58,17 @@ CREATE TABLE `clientes` (
   `Nombre` varchar(120) NOT NULL,
   `Email` varchar(60) NOT NULL,
   `Contraseña` varchar(60) NOT NULL,
-  `Telefono` int(10) NOT NULL,
+  `Telefono` varchar(10) NOT NULL,
   `Direccion` varchar(120) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Volcado de datos para la tabla `clientes`
+--
+
+INSERT INTO `clientes` (`ID_Cliente`, `Nombre`, `Email`, `Contraseña`, `Telefono`, `Direccion`) VALUES
+(2, 'Abraham Flores', 'amflores@gmail.com', 'tkd1', '7224711187', 'Av de los maistros'),
+(3, 'Arely Mejia', 'aremejia@mgmail.com', 'tkd1', '1234567890', 'Por capu ajsjs');
 
 -- --------------------------------------------------------
 
@@ -60,8 +81,19 @@ CREATE TABLE `ordenes` (
   `ID_Cliente` int(11) NOT NULL,
   `Fecha` timestamp NOT NULL DEFAULT current_timestamp(),
   `Total` float NOT NULL,
-  `MetodoPago` char(1) NOT NULL
+  `MetodoPago` char(1) NOT NULL DEFAULT 'T'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Volcado de datos para la tabla `ordenes`
+--
+
+INSERT INTO `ordenes` (`ID_Orden`, `ID_Cliente`, `Fecha`, `Total`, `MetodoPago`) VALUES
+(1, 2, '2023-05-17 08:18:39', 11.98, 'E'),
+(2, 2, '2023-05-19 20:30:28', 19.94, 'T'),
+(3, 3, '2023-05-21 08:21:40', 3.98, 'T'),
+(4, 2, '2023-05-22 12:50:29', 14, 'T'),
+(5, 2, '2023-05-22 13:09:05', 8.6, 'T');
 
 --
 -- Índices para tablas volcadas
@@ -94,19 +126,19 @@ ALTER TABLE `ordenes`
 -- AUTO_INCREMENT de la tabla `catalogo`
 --
 ALTER TABLE `catalogo`
-  MODIFY `ID_Producto` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `ID_Producto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT de la tabla `clientes`
 --
 ALTER TABLE `clientes`
-  MODIFY `ID_Cliente` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `ID_Cliente` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT de la tabla `ordenes`
 --
 ALTER TABLE `ordenes`
-  MODIFY `ID_Orden` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `ID_Orden` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- Restricciones para tablas volcadas
